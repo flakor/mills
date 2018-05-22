0e0d3f3ef820fe63ab477febf46ac3ec16f187c8
pablo <pablo.ortiz.barra@gmail.com>
2018-05-22 12:44:18 -0400

    cambio ruta

--- src/api/models/db.js
+++ src/api/models/db.js
@@ -2,7 +2,7 @@
 var mongoose = require('mongoose');
 var gracefulShutdown;
 const config = require('../../config');
-var dbURI = 'mongodb://mills:mills@ds123490.mlab.com:23490/mean-tasks';
+var dbURI = config.database;
 if (process.env.NODE_ENV === 'production') {
   dbURI = process.env.MONGOLAB_URI;
 }
