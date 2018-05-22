7b4c9a6a1484555d6afaf7cabc4ea2ba1f16b4b2
pablo <pablo.ortiz.barra@gmail.com>
2018-05-22 10:27:56 -0400

    delete cors()

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const cors = require('cors');
+// const cors = require('cors');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const config = require('./config');
@@ -25,7 +25,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(cors());
+// app.use(cors());
 // Enable CORS from client-side
 app.use(function(req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
